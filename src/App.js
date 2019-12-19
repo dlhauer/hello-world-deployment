c19a75895b91f294fdf57f31acc782b4bad48e38
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-19 10:26:02 -0800

    Change header again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Dan is Drinking Tea</h1>
+      <h1>Dan is Drinking Tea !!!! :)</h1>
       <h2>
         Deployed with{' '}
         <a
